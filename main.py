b0ab1334a65b04fdd7547356210a4221b9993db1
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-07-15 23:18:12 -0300

    full refactor for a more pythonic code

--- main.py
+++ main.py
@@ -1,140 +1,100 @@
-#author: "Alan França"
-#author: "Paulo Henrique"
+'''Module for testing purposes.'''
 
-#modules
+__author__ = 'Alan França, Paulo Henrique'
+
+import os
+from functools import partial
+from glob import iglob
+from typing import List
+
+import playsound
 import speech_recognition as sr
 from gtts import gTTS
-import os.path
-import playsound
-import os,shutil
 
-#vars
-scene = "home"
-scene_ = 0
-type_ = ""
-memory = []
-audio_pos = 0
 
-folder = 'sfx/'
-for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-    except Exception as e:
-        print(e)
-
-def falar(text):
-    global audio_pos
-    audio_pos = audio_pos + 1
-    voz = gTTS(text, lang="pt-BR")
-    voz.save(f"sfx/voz{audio_pos}.mp3")
+def speak(text, audio_pos=0):
+    voice = gTTS(text, lang="pt-BR")
+    voice.save(f"sfx/voz{audio_pos}.mp3")
     playsound.playsound(f'sfx/voz{audio_pos}.mp3')
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    while scene == "home":
+
+def ask(r: sr.Recognizer, source: sr.AudioSource, message: str=None):
+    if message:
+        speak(message)
+
+    audio = r.listen(source)
+    return r.recognize_google(audio, language="pt-BR")
+
+
+def ask_operand(r: sr.Recognizer,
+                source: sr.AudioSource,
+                order: str,
+                operation_name: str):
+    while True:
         try:
-            falar("Esperando Comando")
-            #r.adjust_for_ambient_noise(source)
-            audio = r.listen(source)
-            result = r.recognize_google(audio,language="pt-BR")
-            print(result)
-            if "somar" in result:
-                scene = "somar"
-                falar("Comando de Soma Selecionado")
-            elif "multiplicar" in result:
-                scene = "multiplicar"
-            elif "dividir" in result:
-                scene = "dividir"
-            elif "subtrair" in result:
-                scene = "subtrair"
-        except:
-            falar("Comando de voz não reconhecido.")
-    while scene == "somar":
-        try:
-            if scene_ == 0:
-                falar("Fale o primeiro valor para somar")
-            #    r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene_ = 1
-            if scene_ == 1:
-                falar("Fale o segundo valor para somar")
-            #    r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene = "result"
-                type_ = "somar"
-        except:
-            falar("Comando de voz não reconhecido")
-    while scene == "dividir":
-        try:
-            if scene_ == 0:
-                falar("Fale o primeiro valor para dividir")
-                #r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene_ = 1
-            if scene_ == 1:
-                falar("Fale o segundo valor para dividir")
-                #r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene = "result"
-                type_ = "dividir"
-        except:
-            falar("Comando de voz não reconhecido")
-    while scene == "multiplicar":
-        try:
-            if scene_ == 0:
-                falar("Fale o primeiro valor para multiplicar")
-                #r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene_ = 1
-            if scene_ == 1:
-                falar("Fale o segundo valor para multiplicar")
-        #        r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                falar(f"Valor Reconhecido: {int(result)}")
-                memory.append(int(result))
-                scene = "result"
-                type_ = "multiplicar"
-        except:
-            falar("Comando de voz não reconhecido")
-    while scene == "subtrair":
-        try:
-            if scene_ == 0:
-                falar("Fale o primeiro valor para subtrair")
-            #    r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                memory.append(int(result))
-                scene_ = 1
-            if scene_ == 1:
-                print("Fale o segundo valor para subtrair")
-            #    r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                result = r.recognize_google(audio,language="pt-BR")
-                memory.append(int(result))
-                scene = "result"
-                type_ = "subtrair"
-        except:
-            falar("Comando de voz não reconhecido")
-if scene == "result":
-    scene_ = 0
-    if type_ == "somar":
-        falar(f"O Resultado é {int(memory[0]+memory[1])}")
-    elif type_ == "dividir":
-        falar(f"O Resultado é {int(memory[0]/memory[1])}")
-    elif type_ == "multiplicar":
-        falar(f"O Resultado é {int(memory[0]*memory[1])}")
-    elif type_ == "subtrair":
-        falar(f"O Resultado é {int(memory[0]-memory[1])}")
+            return ask(
+                r, source,
+                message=f"Fale o {order} valor para {operation_name}")
+        except sr.UnknownValueError as e:
+            speak("Comando de voz não reconhecido.")
+
+
+def command_arith(operation,
+                  operation_name: str,
+                  r: sr.Recognizer,
+                  source: sr.AudioSource):
+    return operation(*(ask_operand(order, operation_name=operation_name)
+                       for order in ["primeiro", "segundo"]))
+
+
+def ask_command(r: sr.Recognizer,
+                source: sr.AudioSource,
+                commands: List[str]):
+    while True:
+        while True:
+            try:
+                command_name = ask(r, source, message="Esperando Comando")
+            except sr.UnknownValueError as e:
+                speak("Comando de voz não reconhecido.")
+            else:
+                break
+
+        for command in commands:
+            if command in command_name:
+                return command
+        speak("Não consegui entender um comando conhecido.")
+
+
+if __name__ == "__main__":
+    audio_pos = 0
+
+    COMMANDS = {
+            "somar": partial(
+                command_arith,
+                lambda x, y: x + y,
+                "somar"),
+            "dividir": partial(
+                command_arith,
+                lambda x, y: x // y,
+                "dividir"),
+            "multiplicar": partial(
+                command_arith,
+                lambda x, y: x * y,
+                "multiplicar"),
+            "subtrair": partial(
+                command_arith,
+                lambda x, y: x - y,
+                "subtrair"),
+    }
+
+    for path in iglob('sfx/*'):
+        if os.path.isfile(path):
+            os.unlink(path)
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        command = ask_command(r, source, commands=list(COMMANDS))
+        speak(f"Comando {command} selecionado")
+        result = COMMANDS[command](r, source)
+
+    print(f"O resultado é {result}")
